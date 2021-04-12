c8ed6475917074e1d01717e1a68fdd8ea37608be
ashraffares <ashraffares090@gmail.com>
2021-04-12 12:33:57 +0200

    Add today events scope and to index view

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,6 @@ class Event < ApplicationRecord
   has_many :attendees, through: :invitation_lists, source: :user
 
   scope :past, -> { where('start_date < ?', Date.today) }
-  scope :upcoming, -> { where('start_date >= ?', Date.today) }
+  scope :today, -> { where('start_date == ?', Date.today) }
+  scope :upcoming, -> { where('start_date > ?', Date.today) }
 end
