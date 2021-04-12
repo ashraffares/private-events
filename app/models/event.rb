9c72347fa8b5013a7e22d64494c6932126837438
ashraffares <ashraffares090@gmail.com>
2021-04-12 12:22:02 +0200

    Add past and upcomin scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator , class_name: 'User'
   has_many :invitation_lists, dependent: :destroy
   has_many :attendees, through: :invitation_lists, source: :user
+
+  scope :past, -> { where('start_date < ?', Date.today) }
+  scope :upcoming, -> { where('start_date >= ?', Date.today) }
 end
