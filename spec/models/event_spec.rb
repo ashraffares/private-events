d0d3196deac79e36015a518a3965eaea7e9fb0a4
ashraffares <ashraffares090@gmail.com>
2021-04-14 22:28:24 +0200

    Fix rubocop indentation error

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 RSpec.describe Event, type: :model do
   it 'validate name,start_date,end_date,location,description,creator_id' do
-    event = Event.new(name: '', start_date: '', end_date: '', location: '', description: '',creator_id: '')
+    event = Event.new(name: '', start_date: '', end_date: '', location: '', description: '', creator_id: '')
     expect(event.valid?).to be(false)
   end
   describe 'Active recored associations' do
