5b4b6423db2f51158fb0248a7f80d05a08a99c25
ashraffares <ashraffares090@gmail.com>
2021-04-14 21:49:23 +0200

    Add event and invitations test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 require 'spec_helper'
 
 Rspec.describe Event, type: :model do
+    it 'validate name,start_date,end_date,location,description,creator_id' do
+        event = Event.new(name:'Rails odd',start_date:'11/4/2020',end_date:'11/4/2020',location:'Alex',description:'cover rails weird part',creator_id:'1')
+        expect (event.valid?).to be(true)
+    end
+    it 'validate name,start_date,end_date,location,description,creator_id' do
+        event = Event.new(name:'',start_date:'',end_date:'',location:'',description:'',creator_id:'')
+        expect (event.valid?).to be(false)
+    end
     describe 'Active recored associations' do
         it 'belongs_to creator' do
             expect {should belongs_to(:creator).class_name('User')}
