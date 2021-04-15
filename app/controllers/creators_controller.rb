36f2cb056ccc58c015831e4e5460c688f621afeb
ashraffares <ashraffares090@gmail.com>
2021-04-15 20:32:36 +0200

    Add creators controller and info view

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -1,11 +1,7 @@
-class UsersController < ApplicationController
-    def index
+class CreatorsController < ApplicationController
+    def info
         @created_events = current_user.created_events
         @past_events = current_user.attended_event.past
         @upcoming_events = current_user.attended_event.upcoming
-    end
-    private 
-    def event_params
-        params.require(:event).permit(:email, :encrypted_password)
     end
 end
